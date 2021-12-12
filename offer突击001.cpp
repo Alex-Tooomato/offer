4fb5d7cb88f4c7c27e3b2f9eae9715b96600d3a1
Alex_ywh <skyywh2014@163.com>
2021-12-12 21:00:35 +0800

    offer突击001进一步优化

--- offer突击001.cpp
+++ offer突击001.cpp
@@ -16,8 +16,20 @@ public:
         if(b>0)b=-b;
         unsigned int n=0;
         while(a<=b){
-            a-=b;
-            n++;
+            int value=b;
+            // 如果不用 unsigned 的话，那么当 a = -2147483648, b = 1 的时候，k 会越界
+            unsigned int k=1;
+            // 0xc0000000 是十进制 -2^30 的十六进制的表示
+            // 判断 value >= 0xc0000000 的原因：保证 value + value 不会溢出
+            // 可以这样判断的原因是：0xc0000000 是最小值 -2^31 的一半，
+            // 而 a 的值不可能比 -2^31 还要小，所以 value 不可能比 0xc0000000 小
+
+            while(value>=0xc0000000&&a<=value+value){
+                value+=value;
+                k+=k;
+            }
+            a-=value;
+            n+=k;
         }
         return sign == 1 ? n : -n;
     }
