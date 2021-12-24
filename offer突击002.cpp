6010d315dffb1d3249e960fc3a60caf418b51862
Alex_ywh <skyywh2014@163.com>
2021-12-24 21:21:30 +0800

    offer突击002

--- offer突击002.cpp
+++ offer突击002.cpp
@@ -1,4 +1,39 @@
 //
 // Created by skyyw on 2021/11/30.
 //
+#include <string>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string res;
+        reverse(a.begin(),a.end());
+        reverse(b.begin(),b.end());
+        int carry=0;
+        int n=max(a.size(),b.size());
+        for(int i=0;i<n;i++){
+            carry+=i<a.size()?(a.at(i)=='1'):0;
+            carry+=i<b.size()?(b.at(i)=='1'):0;
+            res.push_back((carry%2)?'1':'0');
+            carry/2;
+        }
+        if(carry){
+            res.push_back('1');
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
+int main()
+{
+    Solution su;
+    string a="11";
+    string b="01";
+    string ans=su.addBinary(a,b);
+    cout<<"ans="<<ans<<endl;
+    return 0;
+};
+
 
